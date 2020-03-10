2e84b99a09325d57f2a0385f10619b73091ad850
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-03-11 00:44:23 +0800

    self

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator</h1>
-            {/* <Link to="/">Home</Link> */}
+            <Link to="/">Home</Link>
             {/* <Link to="/otherpage">Other Page</Link> */}
           </header>
           <div>
